7440c6d9a050c68ac3ea100be4bb029cb1a42b5a
jszapat <josephzapata.student@careerdevs.com>
2020-05-26 18:22:29 -0400

    work component folder

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-import './App.css'; 
-
+import './App.css';
+import Resource from './components/Resource';
 
 class App extends Component {
+  resource = {
+    posterName: 'Joseph',
+    resourceAuthor: 'NetNinja',
+    authorSkillLevel: 'Advance',
+    cohort: '0',
+    title: 'React & Redux Complete',
+    categories: ['React', 'Redux'],
+    summary:
+      'A good resource on building a couple react apps with and without redux',
+    link: 'www.google.com/search?q=NetNinja%20React',
+    resourceType: 'Playlist',
+    datePublished: '2018-09-10.00:00:00.000z',
+    videoLength: null,
+    timeToComplete: 300,
+    rating: 5,
+    comments: [
+      { user: 'Joe', text: 'Pretty good resource' },
+      { user: 'Peter', text: 'A great resource to learn React.' }
+    ]
+  }
+
+
+
   render() {
     return (
-      <div className="App">
+      <div className='App'>
         <h1>Welcome to BrainHive!</h1>
+        <Resource  resource={this.resource} />
+        <Resource  resource={this.resource} />
+        <Resource  resource={this.resource} />
       </div>
-    )
+    );
   }
 }
-  
+
 export default App;
--- src/components/Resource.js
+++ src/components/Resource.js
@@ -1,19 +1,19 @@
-import React from "react";
+import React from 'react';
 // import App from "../App";
 // import resource from './components/Resource';
 
-const Resource = ({resource}) => {
-   
-    return(
-        <div className={'box'}>
-            <h2>{resource.title}</h2>
-            <p>{resource.author}</p>
-            <p>Published on: {resource.datePublished}</p>
-            { resource.videoLength ? (<p>Length: {resource.videoLength} minutes</p>) : null }
-            {/*Ternary function: conditional ? true code :  false code */}
-          <p>Comments: {resource.comments.length}</p>
-    
-        </div>
-    )
-}
+const Resource = ({ resource }) => {
+  return (
+    <div className={'box'}>
+      <h2>{resource.title}</h2>
+      <p>{resource.author}</p>
+      <p>Published on: {resource.datePublished}</p>
+      {resource.videoLength ? (
+        <p>Length: {resource.videoLength} minutes</p>
+      ) : null}
+      {/* conditional ? true code : false code */}
+      <p>Comments: {resource.comments.length}</p>
+    </div>
+  );
+};
 export default Resource;
